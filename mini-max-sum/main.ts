adcdc37dbeff47a68afd8581e92322a4205609dc
Your Name <you@example.com>
2023-02-02 21:38:14 +0600

    9th problem solved!

---
+++ mini-max-sum/main.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr: number[]): void {
+    let max = Math.max(...arr)
+    let min = Math.min(...arr)
+    let sum = arr.reduce((partialSum, a) => partialSum + a, 0)
+    
+    console.log(sum - max, sum - min)
+}
+
+function main() {
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
