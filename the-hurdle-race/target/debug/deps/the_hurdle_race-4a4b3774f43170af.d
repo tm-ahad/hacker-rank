f6d760052d04a116d165458dc8ae90029e1349dd
Your Name <you@example.com>
2023-02-16 21:23:07 +0600

    The hurdle race.

---
+++ the-hurdle-race/target/debug/deps/the_hurdle_race-4a4b3774f43170af.d
@@ -0,0 +1,5 @@
+/media/ahad/Resources/GoProjects/hacker-rank/the-hurdle-race/target/debug/deps/the_hurdle_race-4a4b3774f43170af.rmeta: src/main.rs
+
+/media/ahad/Resources/GoProjects/hacker-rank/the-hurdle-race/target/debug/deps/the_hurdle_race-4a4b3774f43170af.d: src/main.rs
+
+src/main.rs:
