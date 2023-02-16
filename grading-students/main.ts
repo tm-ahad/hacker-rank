c49bde2020999c976654b9ae0819149150b1e886
Your Name <you@example.com>
2023-02-16 22:04:03 +0600

    Grading students.

---
+++ grading-students/main.ts
@@ -0,0 +1,75 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades: number[]): number[] {
+    let res: number[] = [];
+    let len = grades.length
+    
+    for (let i = 0; i < len; i++){
+        let e = grades[i];
+        let pos = e - e%5 + 5;
+        
+        if (e < 5) {
+            continue;
+        } else if (e < 38 && pos < 40) {
+            res.push(e);
+        } else {
+            
+            if (3 > Math.abs(pos - e)) {   
+                res.push(pos);
+            } else { res.push(e) }
+        }
+    }
+    
+    return res
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const gradesCount: number = parseInt(readLine().trim(), 10);
+
+    let grades: number[] = [];
+
+    for (let i: number = 0; i < gradesCount; i++) {
+        const gradesItem: number = parseInt(readLine().trim(), 10);
+
+        grades.push(gradesItem);
+    }
+
+    const result: number[] = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+
