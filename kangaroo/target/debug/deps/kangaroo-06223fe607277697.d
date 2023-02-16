2faedc6d8ac8eddc42659da13e6dcc80f85787ff
Your Name <you@example.com>
2023-02-16 21:57:16 +0600

    2 more

--- kangaroo/target/debug/deps/kangaroo-06223fe607277697.d
+++
@@ -1,5 +0,0 @@
-/media/ahad/Resources/GoProjects/hacker-rank/kangaroo/target/debug/deps/kangaroo-06223fe607277697.rmeta: src/main.rs
-
-/media/ahad/Resources/GoProjects/hacker-rank/kangaroo/target/debug/deps/kangaroo-06223fe607277697.d: src/main.rs
-
-src/main.rs:
--- kangaroo/target/debug/deps/kangaroo-61ed75870a9bee5c.d
+++
@@ -1,5 +0,0 @@
-/media/ahad/Resources/GoProjects/hacker-rank/kangaroo/target/debug/deps/kangaroo-61ed75870a9bee5c.rmeta: src/main.rs
-
-/media/ahad/Resources/GoProjects/hacker-rank/kangaroo/target/debug/deps/kangaroo-61ed75870a9bee5c.d: src/main.rs
-
-src/main.rs:
