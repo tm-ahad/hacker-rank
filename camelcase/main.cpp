38fc3afdd61e542e3b94e98ceb050e6dd8adab0e
tm-ahad <tm.ahad.07@gmail.com>
2023-10-20 20:48:37 +0600

    camelcase done.

---
+++ camelcase/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int ret = 1;
+    
+    for (char &c: s) {
+        int ascii = (int)c;
+        
+        if (ascii > 64 && ascii < 91) {
+            ret++;
+        }
+    }
+    
+    return ret;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
